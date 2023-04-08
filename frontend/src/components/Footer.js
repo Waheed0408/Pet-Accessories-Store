95e44cc83eb2111a1e61386a7d0e87b64f4f5952
Waheed0408 <wmoonwar@gmail.com>
2023-04-09 01:22:30 +0600

    Pet Accessories Store

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          
+          <p>Contact Us: 01616888175 | Email Us:  <a href={`mailto:waheed.moonwar@g.bracu.ac.bd.email`}>waheed.moonwar@g.bracu.ac.bd</a></p>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Pet Accessories Store',
+  description: 'Find the best Pet Care Items for cheap',
+  keywords: 'pet food, pet medicines, pet clothing',
+}
+
+export default Meta
